e8ab5893708188edf4eabaf7c81c4667234540f5
Andre van Tonder <drevtonder@gmail.com>
2018-03-30 08:23:02 +0800

    Basic Structure

---
+++ BL/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Item
+    {
+        public bool OnOrder { get; set; }
+        public string Description { get; set; }
+        public int CurrentCount { get; set; }
+        public string ItemCode { get; set; }
+    }
+}
